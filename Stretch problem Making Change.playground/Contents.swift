efdb015aa8ee09c4f14ad17cf930e766fda59c54
Eva <Bresciano>
2016-05-30 09:31:53 -0600

    stretch

---
+++ Stretch problem Making Change.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var penny = Int()
+var nickle = Int()
+var dime = Int()
+var quarter = Int()
+var dollar = Int()
+
+let moneyArray = [penny, nickle, dime, quarter, dollar]
+
+func moneyCount(money: Int) -> String {
+    
+    let strings = moneyArray.map { (money) -> String in
+        var money = money
+        var output = ""
+        while money > 0 {
+            output = 
+            
+        }
+        return output
+    }
+}
+    
+    
+    
